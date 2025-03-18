2b766252bef6db93cdb1e6eb7ea6da3772cf3a21
pouria-drd <pouriadrdgmail.com>
2025-03-18 13:24:53 +0330

    improving ci cd

--- app/(general)/page.tsx
+++ app/(general)/page.tsx
@@ -2,7 +2,7 @@ function Homepage() {
     return (
         <div>
             <h1>سلام و درود فراوان</h1>
-            <p>pouria.drd v2</p>
+            <p>pouria.drd v3</p>
         </div>
     );
 }
