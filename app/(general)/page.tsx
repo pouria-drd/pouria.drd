56cd45ab4aa9f038c9085cab2097e1cdde4caf59
pouria-drd <pouriadrdgmail.com>
2025-03-18 14:29:45 +0330

    testing workflow speed

--- app/(general)/page.tsx
+++ app/(general)/page.tsx
@@ -1,9 +1,10 @@
 function Homepage() {
     return (
-        <div>
+        <div
+            className="bg-white rounded-lg p-4
+            flex flex-col items-center justify-center">
             <h1>سلام و درود فراوان</h1>
-            <p>pouria.drd v6</p>
-            <h1>سلام و درود فراوان</h1>
+            <p>pouria.drd v6.5</p>
         </div>
     );
 }
