1a2393bcb1005a7b538c6411d33bfd7076d43278
pouria-drd <pouriadrdgmail.com>
2025-03-18 12:44:03 +0330

    navbar style improved

--- components/layouts/navigation/navbar/Navbar.tsx
+++ components/layouts/navigation/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ interface NavbarProps {
 }
 
 const Navbar = (props: NavbarProps) => {
-    const [isScrolled, isScrollingDown] = useScroll(50, 50);
+    const [isScrollingDown] = useScroll(50, 50);
 
     function handleSidebarButtonClick() {
         props.onOpenSidebar();
@@ -22,16 +22,14 @@ const Navbar = (props: NavbarProps) => {
             initial={{ y: 0 }}
             animate={{
                 y: isScrollingDown ? "-100%" : 0,
-                backgroundColor: isScrolled
-                    ? "rgba(239, 244, 255, 0.95)" // dark background with opacity 0.9 when scrolled
-                    : "rgba(239, 244, 255, 0)", // transparent background when not scrolled
             }}
             transition={{
                 type: "ease-in-out",
                 stiffness: 300,
                 damping: 25,
             }}
-            className={`sticky top-0 z-10 glass-effect app-px 
+            className={`bg-drd-primary-50/90
+            sticky top-0 z-10 glass-effect app-px
             flex items-center justify-between p-4`}>
             {/* Logo */}
             <PouriaLogo />
