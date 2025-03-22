6b822ca276fad8ec146bbab18e17c1b13aae20cc
pouria-drd <pouriadrdgmail.com>
2025-03-22 19:37:06 +0330

    improved home page and image changer style, added more image

--- app/(general)/page.tsx
+++ app/(general)/page.tsx
@@ -1,19 +1,26 @@
+import { Button } from "@/components/ui";
 import { ImageChanger, SkillsWriter } from "@/components/shared";
 
 const Homepage = () => {
     const skills = ["Vue.js", "React", "Next.js", "Django", "Unity"];
-    const images = ["/images/pouria/pouria.png", "/images/pouria/pouria-2.png"];
+    const images = [
+        "/images/pouria/pouria.png",
+        "/images/pouria/pouria-2.png",
+        "/images/pouria/pouria-3.png",
+    ];
 
     return (
-        <div
-            className="flex flex-col items-center justify-center gap-8
-            w-full py-4 sm:py-8 h-[80dvh] sm:h-auto">
+        <div className="flex flex-col items-center justify-center gap-4 w-full py-4">
             <ImageChanger images={images} />
 
-            <div className="flex flex-col items-center text-center gap-2 w-full">
-                <h1 className="title-medium">پوریا دارندی</h1>
+            <div className="flex flex-col items-center text-center gap-2 w-full mt-4 r2l">
+                <h1
+                    className="text-drd-neutral-950 text-3xl
+                    sm:text-3xl md:text-4xl lg:text-4xl font-bold">
+                    پوریا دارندی
+                </h1>
                 <SkillsWriter skills={skills} />
-                <p className="relative text-p-medium text-center leading-relaxed r2l max-w-2xl">
+                <p className="relative text-p-medium text-center leading-relaxed max-w-2xl">
                     متولد ۱۴ بهمن ۱۳۸۰، برنامه‌نویسی را از دی ۱۳۹۹ به صورت
                     خودآموز با پایتون شروع کردم. سپس از تیر ۱۴۰۰ تا دی ۱۴۰۱ در
                     مجتمع فنی تهران، دوره‌های الگوریتم‌های C++ و پایتون را با
@@ -23,6 +30,10 @@ const Homepage = () => {
                     فراییت با Django، React، Next.js و Vue.js کار کردم و از
                     مرداد ۱۴۰۳ در شرکت عصر دانش‌افزار مشغول به کار هستم.
                 </p>
+            </div>
+
+            <div className="mb-2">
+                <Button href="/contact-me">ارسال پیام</Button>
             </div>
         </div>
     );
--- components/shared/ImageChanger.tsx
+++ components/shared/ImageChanger.tsx
@@ -3,13 +3,16 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import { cn } from "@/utils";
 
 interface ImageChangerProps {
     images: string[];
     interval?: number; // Optional prop for flexibility
+    className?: string;
 }
 
-const ImageChanger = ({ images, interval = 3000 }: ImageChangerProps) => {
+const ImageChanger = (props: ImageChangerProps) => {
+    const { images, interval = 5000 } = props;
     const [index, setIndex] = useState(0);
 
     useEffect(() => {
@@ -23,7 +26,13 @@ const ImageChanger = ({ images, interval = 3000 }: ImageChangerProps) => {
     }, [images, interval]);
 
     return (
-        <div className="bg-white relative size-72 sm:size-80 rounded-full overflow-hidden shadow-2xl">
+        <div
+            className={cn(
+                `bg-gradient-to-b from-white to-drd-primary-75
+                relative size-72 sm:size-80 min-w-72 min-h-72
+                rounded-[50%] overflow-hidden shadow-2xl aspect-square`,
+                props.className
+            )}>
             <AnimatePresence mode="wait">
                 <motion.div
                     key={index}
@@ -31,19 +40,19 @@ const ImageChanger = ({ images, interval = 3000 }: ImageChangerProps) => {
                     animate={{ opacity: 1, scale: 1, x: 0, rotate: 0 }}
                     exit={{ opacity: 0, scale: 1.1, x: -50, rotate: 10 }}
                     transition={{
-                        duration: 1,
-                        ease: "easeInOut",
+                        duration: 0.5,
+                        damping: 15,
                         type: "spring",
                         stiffness: 100,
-                        damping: 15,
+                        ease: "easeInOut",
                     }}
-                    className="absolute inset-0">
+                    className="absolute inset-0 w-full">
                     <Image
                         fill
                         priority
                         src={images[index]}
                         alt="Pouria Darandi"
-                        className="object-cover"
+                        className="object-cover w-full"
                     />
                 </motion.div>
             </AnimatePresence>
