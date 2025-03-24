f4b0b8c6fdb324047aab3bb7be1e114136a52f2f
pouria-drd <pouriadrdgmail.com>
2025-03-24 14:39:43 +0330

    fixed project stle py

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -18,7 +18,7 @@ function ProjectsPage() {
     });
 
     return (
-        <div className="min-h-dvh py-10">
+        <div className="min-h-dvh py-4 sm:py-8">
             <div className="max-w-6xl mx-auto">
                 <h1 className="text-3xl font-bold text-center text-drd-primary-900">
                     پروژه‌های من
